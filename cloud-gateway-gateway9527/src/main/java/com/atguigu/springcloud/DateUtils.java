8c1b9a9ae3565f5fe4b155b5d1266c16078dcf3d
xiangwj <wenjunxp@sina.com>
2021-11-15 21:39:45 +0800

    Predicate After

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/DateUtils.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import java.time.ZonedDateTime;
+
+public class DateUtils {
+	public static void main(String[] args) {
+		ZonedDateTime zbj = ZonedDateTime.now();
+		System.out.println(zbj);
+				
+	}
+}
